a9e0ffbcad8f9bfe1afec18a2b2e3f4e3bc2a165
alnabid. <alnabidrm@gmail.com>
2024-02-16 15:42:04 +0600

    Commit : implemented automatic numbering feature

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,26 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true,
 });
+
+let orderNumbers = [
+    'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth',
+    'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth', 'twentieth',
+    'twenty-first', 'twenty-second', 'twenty-third', 'twenty-fourth', 'twenty-fifth', 'twenty-sixth', 'twenty-seventh', 'twenty-eighth', 'twenty-ninth', 'thirtieth',
+    'thirty-first', 'thirty-second', 'thirty-third', 'thirty-fourth', 'thirty-fifth', 'thirty-sixth', 'thirty-seventh', 'thirty-eighth', 'thirty-ninth', 'fortieth',
+    'forty-first', 'forty-second', 'forty-third', 'forty-fourth', 'forty-fifth', 'forty-sixth', 'forty-seventh', 'forty-eighth', 'forty-ninth', 'fiftieth',
+    'fifty-first', 'fifty-second', 'fifty-third', 'fifty-fourth', 'fifty-fifth', 'fifty-sixth', 'fifty-seventh', 'fifty-eighth', 'fifty-ninth', 'sixtieth',
+    'sixty-first', 'sixty-second', 'sixty-third', 'sixty-fourth', 'sixty-fifth', 'sixty-sixth', 'sixty-seventh', 'sixty-eighth', 'sixty-ninth', 'seventieth',
+    'seventy-first', 'seventy-second', 'seventy-third', 'seventy-fourth', 'seventy-fifth', 'seventy-sixth', 'seventy-seventh', 'seventy-eighth', 'seventy-ninth', 'eightieth',
+    'eighty-first', 'eighty-second', 'eighty-third', 'eighty-fourth', 'eighty-fifth', 'eighty-sixth', 'eighty-seventh', 'eighty-eighth', 'eighty-ninth', 'ninetieth',
+    'ninety-first', 'ninety-second', 'ninety-third', 'ninety-fourth', 'ninety-fifth', 'ninety-sixth', 'ninety-seventh', 'ninety-eighth', 'ninety-ninth', 'hundredth'
+];
+
+const hasAutoNumber = Array.from(document.querySelectorAll('.has-auto-number'));
+
+hasAutoNumber.forEach(n => {
+    const orderNumberElements = Array.from(n.querySelectorAll('.order-number'));
+    for (let r = 0; r < orderNumberElements.length; r++) {
+        orderNumberElements[r].innerText = orderNumbers[r] + ' review';
+        orderNumberElements[r].style.textTransform = 'uppercase';
+    }
+});
