a3cdbb4db923f10502fba87945f55ad1f795ce7e
hasethakuru <hassanthakuru@proton.me>
2025-01-18 02:19:18 +0530

    Disable scrolling on mobile menu

--- public/js/main.js
+++ public/js/main.js
@@ -80,8 +80,13 @@ window.addEventListener('load', initializeReadMore)
 $('.a-nav-toggle, .menu-main a').on('click', function () {
     if ($('html').hasClass('body-menu-opened')) {
         $('html').removeClass('body-menu-opened').addClass('body-menu-close');
+
+        document.body.style.position = '';
+        
     } else {
         $('html').addClass('body-menu-opened').removeClass('body-menu-close');
+
+        document.body.style.position = 'fixed';
     }
 });
 
