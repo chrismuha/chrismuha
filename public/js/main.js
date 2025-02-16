f4794d31f2870f0cf38cd230d4426caf5fa2961d
hasethakuru <hassanthakuru@proton.me>
2025-02-16 13:53:42 +0530

    Bug fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,100 +1,101 @@
 // DOM ELEMENTS
-const newsBannerSection = document.querySelector('.news-banner');
-const newsBannerText = document.querySelector('.breaking-news-headline');
-var loader = document.getElementById("preloader");
-const circleTopButton = document.getElementById("top");
+// const newsBannerSection = document.querySelector('.news-banner');
+// const newsBannerText = document.querySelector('.breaking-news-headline');
+// var loader = document.getElementById("preloader");
+// const circleTopButton = document.getElementById("top");
 
-newsBannerText.innerText = ''
-
-window.addEventListener("load", function () {
-    loader.style.display = "none";
-})
-
-window.addEventListener('scroll', function (event) {
-    circleTopButton.style.visibility = "hidden";
-});
-window.addEventListener('scrollend', function (event) {
-    circleTopButton.style.visibility = "unset";
-});
+// newsBannerText.innerText = '';
 
 
-circleTopButton.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-    circleTopButton.classList.add('hover-animation');
-    setTimeout(() => {
-        circleTopButton.classList.remove('hover-animation');
-    }, 500);
-})
+// window.addEventListener("load", function () {
+//     loader.style.display = "none";
+// })
 
-function addReadMoreToElement(element) {
-    const maxLength = 100;
+// window.addEventListener('scroll', function (event) {
+//     circleTopButton.style.visibility = "hidden";
+// });
+// window.addEventListener('scrollend', function (event) {
+//     circleTopButton.style.visibility = "unset";
+// });
 
-    if (element.dataset.processed) return;
 
-    const originalContent = element.innerHTML.trim();
+// circleTopButton.addEventListener('click', () => {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+//     circleTopButton.classList.add('hover-animation');
+//     setTimeout(() => {
+//         circleTopButton.classList.remove('hover-animation');
+//     }, 500);
+// })
 
-    if (originalContent.length > maxLength) {
-        const truncatedContent = originalContent.slice(0, maxLength) + '...';
+// function addReadMoreToElement(element) {
+//     const maxLength = 100;
 
-        const truncatedSpan = document.createElement('span');
-        truncatedSpan.innerHTML = truncatedContent;
+//     if (element.dataset.processed) return;
 
-        const fullSpan = document.createElement('span');
-        fullSpan.innerHTML = originalContent;
-        fullSpan.style.display = 'none';
+//     const originalContent = element.innerHTML.trim();
 
-        const readMoreLink = document.createElement('a');
-        readMoreLink.href = '#';
-        readMoreLink.classList.add('read-more-link')
-        readMoreLink.textContent = 'Read more';
-        readMoreLink.style.marginLeft = '5px';
+//     if (originalContent.length > maxLength) {
+//         const truncatedContent = originalContent.slice(0, maxLength) + '...';
 
-        readMoreLink.addEventListener('click', (e) => {
-            e.preventDefault();
-            truncatedSpan.style.display = 'none';
-            fullSpan.style.display = 'inline';
-            readMoreLink.style.display = 'none';
-        });
+//         const truncatedSpan = document.createElement('span');
+//         truncatedSpan.innerHTML = truncatedContent;
 
-        element.innerHTML = '';
-        element.appendChild(truncatedSpan);
-        element.appendChild(fullSpan);
-        element.appendChild(readMoreLink);
-    }
+//         const fullSpan = document.createElement('span');
+//         fullSpan.innerHTML = originalContent;
+//         fullSpan.style.display = 'none';
 
-    element.dataset.processed = 'true';
-}
+//         const readMoreLink = document.createElement('a');
+//         readMoreLink.href = '#';
+//         readMoreLink.classList.add('read-more-link')
+//         readMoreLink.textContent = 'Read more';
+//         readMoreLink.style.marginLeft = '5px';
 
-function initializeReadMore() {
-    const elements = document.querySelectorAll('.read-more');
-    elements.forEach(addReadMoreToElement);
-}
+//         readMoreLink.addEventListener('click', (e) => {
+//             e.preventDefault();
+//             truncatedSpan.style.display = 'none';
+//             fullSpan.style.display = 'inline';
+//             readMoreLink.style.display = 'none';
+//         });
 
-document.addEventListener('DOMContentLoaded', initializeReadMore);
-document.onload = initializeReadMore;
-window.addEventListener('load', initializeReadMore)
+//         element.innerHTML = '';
+//         element.appendChild(truncatedSpan);
+//         element.appendChild(fullSpan);
+//         element.appendChild(readMoreLink);
+//     }
 
-$('.a-nav-toggle, .menu-main a').on('click', function () {
-    if ($('html').hasClass('body-menu-opened')) {
-        $('html').removeClass('body-menu-opened').addClass('body-menu-close');
+//     element.dataset.processed = 'true';
+// }
 
-        document.body.style.position = '';
+// function initializeReadMore() {
+//     const elements = document.querySelectorAll('.read-more');
+//     elements.forEach(addReadMoreToElement);
+// }
+
+// document.addEventListener('DOMContentLoaded', initializeReadMore);
+// document.onload = initializeReadMore;
+// window.addEventListener('load', initializeReadMore)
+
+// $('.a-nav-toggle, .menu-main a').on('click', function () {
+//     if ($('html').hasClass('body-menu-opened')) {
+//         $('html').removeClass('body-menu-opened').addClass('body-menu-close');
+
+//         document.body.style.position = '';
         
-    } else {
-        $('html').addClass('body-menu-opened').removeClass('body-menu-close');
+//     } else {
+//         $('html').addClass('body-menu-opened').removeClass('body-menu-close');
 
-        document.body.style.position = 'fixed';
-    }
-});
+//         document.body.style.position = 'fixed';
+//     }
+// });
 
 
-if (newsBannerText.innerText == false) {
-    newsBannerSection.style.display = 'none';
-} else {
-    newsBannerSection.style.display = 'block';
-}
+// if (newsBannerText.innerText == false) {
+//     newsBannerSection.style.display = 'none';
+// } else {
+//     newsBannerSection.style.display = 'block';
+// }
 
-console.log('hey 2')
+// console.log('hey 2')
